b64062851ee8773aa457e6809ca6456242a1aa93
Vibha R M <tmanikandan.bsccs@gmail.com>
2022-08-15 22:39:20 +0530

    First Basic commit for Spring Data Jpa

---
+++ src/main/java/com/heberian/spring/data/jpa/workouts/SpringDataJpaWorkoutsApplication.java
@@ -0,0 +1,13 @@
+package com.heberian.spring.data.jpa.workouts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDataJpaWorkoutsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDataJpaWorkoutsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/heberian/spring/data/jpa/workouts/SpringDataJpaWorkoutsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.heberian.spring.data.jpa.workouts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDataJpaWorkoutsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
